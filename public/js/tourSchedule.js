6fa8c6e8f8ac5021561f23246316092188eb2d22
AntoineGhysens <antoine.ghsens@gmail.com>
2021-10-13 16:16:47 +0200

    commented out script because it's not ready yet

--- public/js/tourSchedule.js
+++ public/js/tourSchedule.js
@@ -1,4 +1,4 @@
-let articlesHeight = []
+let articlesHeight = [];
 let article0 = articlesHeight.push(document.querySelectorAll(".tour-schedule__dates-for-month__available-dates__item")[0].clientHeight);
 let article1 = articlesHeight.push(document.querySelectorAll(".tour-schedule__dates-for-month__available-dates__item")[1].clientHeight);
 let article2 = articlesHeight.push(document.querySelectorAll(".tour-schedule__dates-for-month__available-dates__item")[2].clientHeight);
@@ -7,23 +7,25 @@ let article4 = articlesHeight.push(document.querySelectorAll(".tour-schedule__da
 console.log(articlesHeight)
 
 
+
 let line0 = document.querySelector(".tour-schedule__dates-for-month__progress-bar-container__progress-bar__line");
 let line1 = document.querySelectorAll(".tour-schedule__dates-for-month__progress-bar-container__progress-bar__line")[1];
 let line2 = document.querySelectorAll(".tour-schedule__dates-for-month__progress-bar-container__progress-bar__line")[2];
 let line3 = document.querySelectorAll(".tour-schedule__dates-for-month__progress-bar-container__progress-bar__line")[3];
 
-//margin;
-//date;
 let tourScheduleArticleWithMargin = document.querySelector(".tour-schedule__dates-for-month__available-dates__item + .tour-schedule__dates-for-month__available-dates__item");
-let date = document.querySelector(".tour-schedule__dates-for-month__available-dates__item__date");
-let marginHeading = document.querySelector(".tour-schedule__dates-for-month__available-dates__item__heading");
 let compStyles = window.getComputedStyle(tourScheduleArticleWithMargin);
 let mgtop = parseInt(compStyles.getPropertyValue('margin-top'))
+let date = document.querySelector(".tour-schedule__dates-for-month__available-dates__item__date");
 let compStyles2 = window.getComputedStyle(date);
-let fontSize = parseInt(compStyles2.getPropertyValue('font-size'))
-let headingHeight = parseInt(compStyles2.getPropertyValue('font-size'))
+let dateHeight = parseInt(compStyles2.getPropertyValue('height'))
+let marginHeading = document.querySelector(".tour-schedule__dates-for-month__available-dates__item__heading");
 let compStyles3 = window.getComputedStyle(marginHeading);
 let marginBottomHeading = parseInt(compStyles2.getPropertyValue('margin-bottom'))
+let heading = document.querySelectorAll(".tour-schedule__dates-for-month__available-dates__item__heading")[0];
+let compStyles4 = window.getComputedStyle(heading);
+let headingFontSize = parseInt(compStyles4.getPropertyValue('font-size'))
+
 
 
 //reset
@@ -33,36 +35,22 @@ let marginBottomHeading = parseInt(compStyles2.getPropertyValue('margin-bottom')
 // line3.style.height = "10px"
 // line3.style.height = `${mgtop}`
 //////////////////
-console.log(fontSize)
+// console.log(fontSize)
 console.log(marginBottomHeading)
 
 console.log(articlesHeight[0])
-line0.style.height = articlesHeight[0] + mgtop - marginBottomHeading - 0.75 * fontSize - headingHeight + "px"
-// console.log(linesHeight[0])
-line1.style.height = articlesHeight[1] + mgtop - marginBottomHeading - 0.75 * fontSize - headingHeight + "px" `redefine heading height`
-line2.style.height = articlesHeight[2] + mgtop - marginBottomHeading - 0.75 * fontSize - headingHeight + "px"
-line3.style.height = articlesHeight[3] + mgtop+ mgtop - marginBottomHeading - 0.75 * fontSize - headingHeight + "px"
+line0.style.height = articlesHeight[0] + mgtop  - dateHeight - marginBottomHeading - (0.75 * headingFontSize) + "px"
+line1.style.height = articlesHeight[1] + mgtop - dateHeight - marginBottomHeading - (0.75 * headingFontSize) + "px" //`redefine heading height`
+line2.style.height = articlesHeight[2] + mgtop - marginBottomHeading - (0.75 * headingFontSize) + "px"
+line3.style.height = articlesHeight[3] + mgtop - dateHeight - marginBottomHeading - (0.75 * headingFontSize) + "px"
 // let linesHeight = [line0.clientHeight, line1.clientHeight, line2.clientHeight, line3.clientHeight]
 console.log(line0.clientHeight)
-console.log(articlesHeight[0] + mgtop - fontSize)
+// console.log(articlesHeight[0] + mgtop - fontSize)
 console.log(typeof fontSize)
 console.log(typeof marginBottomHeading)
 console.log(typeof mgtop)
 console.log(typeof articlesHeight)
-
-
-
-// for(element of articlesHeight){
-//   for(i = 0; i < 5; i++){
-//
-//   }
-// }
-  // element.style.height = article0.clientHeight
-
-  // for(i = 0; i < element-1; i++){
-  //   console.log(element)
-  //   element.style.height = `article${i}.clientHeight`
-  // }
+console.log(typeof heading1)
 
 
 
