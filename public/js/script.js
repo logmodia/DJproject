c8493feec69b9cfcd9967ed80d75aa58b01c2a57
ThomasBackers <thomasbackers@gmail.com>
2021-10-13 10:54:54 +0200

    on the high speed highway to make the homepage responsive

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,31 @@
 // ---------- CONSTANTS ----------
-const partnersSliderControllers = document.querySelectorAll(".partners-sponsors__contents__interactive-block__slider__controller")
+const partnersSliderControllers = [...document.querySelectorAll(".partners-sponsors__contents__interactive-block__slider__controller")];
+const partners = [...document.querySelectorAll(".partners-sponsors__contents__interactive-block__partners-list__partner")];
 
 // ---------- FUNCTIONS ----------
 const slide = e => {
+    let partnerIndex = 0;
     if (e.target.classList.contains("partners-sponsors__contents__interactive-block__slider__controller--active")) {}
-    else for (let sliderController of partnersSliderControllers) {
-        if (sliderController.classList.contains("partners-sponsors__contents__interactive-block__slider__controller--active")) {
-            sliderController.classList.remove("partners-sponsors__contents__interactive-block__slider__controller--active");
-            e.target.classList.add("partners-sponsors__contents__interactive-block__slider__controller--active");
+    else {
+        for (let sliderController of partnersSliderControllers) {
+            if (sliderController.classList.contains("partners-sponsors__contents__interactive-block__slider__controller--active")) {
+                sliderController.classList.remove("partners-sponsors__contents__interactive-block__slider__controller--active");
+            }
         }
+        e.target.classList.add("partners-sponsors__contents__interactive-block__slider__controller--active");
+        for (let sliderController of partnersSliderControllers) {
+            if (sliderController.classList.contains("partners-sponsors__contents__interactive-block__slider__controller--active")) {
+                partnerIndex = partnersSliderControllers.indexOf(sliderController);
+                break;
+            }
+        }
+        for (let partner of partners) {
+            if (!partner.classList.contains("partners-sponsors__contents__interactive-block__partners-list__partner--hidden")) {
+                partner.classList.add("partners-sponsors__contents__interactive-block__partners-list__partner--hidden");
+                break;
+            }
+        }
+        partners[partnerIndex].classList.remove("partners-sponsors__contents__interactive-block__partners-list__partner--hidden");
     }
 };
 
