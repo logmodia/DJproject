eec987c9f566d5bbab93e1a40ea8b30373e368db
ThomasBackers <thomasbackers@gmail.com>
2021-10-13 15:27:21 +0200

    index.html linked to pages

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,8 +1,11 @@
 // ---------- CONSTANTS ----------
 const partnersSliderControllers = [...document.querySelectorAll(".partners-sponsors__contents__interactive-block__slider__controller")];
 const partners = [...document.querySelectorAll(".partners-sponsors__contents__interactive-block__partners-list__partner")];
+const playButton = document.querySelector(".fa-play");
+const currentTime = document.querySelector(".header__new-single__contents__audio-player__timer__current-time");
 
 // ---------- FUNCTIONS ----------
+//PARTNERS SLIDER
 const slide = e => {
     let partnerIndex = 0;
     if (e.target.classList.contains("partners-sponsors__contents__interactive-block__slider__controller--active")) {}
@@ -29,7 +32,25 @@ const slide = e => {
     }
 };
 
+//AUDIO PLAYER
+const playEvent = e => {
+    if (e.target.classList.contains("fa-play")) {
+        e.target.classList.add("fa-pause");
+        e.target.classList.remove("fa-play");
+    } else {
+        e.target.classList.add("fa-play");
+        e.target.classList.remove("fa-pause");
+    }
+}
+
+//AUDIO PROGRESS BAR
+
+
 // ---------- EVENT LISTENERS ----------
+//PARTNERS SLIDER
 for (let sliderController of partnersSliderControllers) {
     sliderController.addEventListener("click", slide);
 }
+
+//AUDIO PLAYER
+playButton.addEventListener("click", playEvent);
